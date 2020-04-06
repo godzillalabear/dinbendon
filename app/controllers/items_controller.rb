0f2c8f15e4c53a41ee33ab244822a88aa8be817d
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-04-06 11:37:20 +0800

    render form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,15 @@
 class ItemsController < ApplicationController
 
-
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
+                            # excepte:[ :index, :new, :create]
 
   def index
     @items = Item.all
   end
 
   def show
-    @item = Item.find(params[:id])
+    # find_item
+    # @item = Item.find(params[:id])
   end
 
   def new
@@ -15,7 +17,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
+    # @item = Item.new(item_params)
 
     if @item.save
       redirect_to items_path, notice: 'Add new order!'
@@ -25,12 +27,14 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
+    # find_item
+    # @item = Item.find(params[:id])
   end
 
   def update
     #no need a page, just complete the action
-    @item = Item.find(params[:id])
+    # find_item
+    # @item = Item.find(params[:id])
 
     if @item.update(item_params)
       redirect_to items_path, notice: 'Your order is updated!'
@@ -40,12 +44,16 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    find_item
+    # item = Item.find(params[:id])
+    @item.destroy
     redirect_to items_path, notice: 'Delete the order'
   end
 
   private
+  def find_item
+    @item = Item.find(params[:id])
+  end
   def item_params
     params.require(:item).permit(:name, 
                                  :description, 
