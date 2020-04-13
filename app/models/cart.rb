fad5cd663f5232f70f3237be184ebde024e01366
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-04-13 17:19:16 +0800

    WIP - rspec basic function: add amount not items

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,10 +1,20 @@
 # cart.rb
 
 class Cart
+  def initialize
+    @items = []
+  end
+
   def add_item(item_id)
+    @items << item_id
   end
 
   def empty?
     false
+    @items.empty?
+  end
+
+  def items
+    @items
   end
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -14,7 +14,19 @@ RSpec.describe Cart, type: :model do
       #Assert
       expect(cart.empty?).to be false
     end
+
+    it "when add same product, only amount changes, not items" do
+      cart = Cart.new
+
+      3.times { cart.add_item(1) }
+      2.times { cart.add_item(2) }
+      2.times { cart.add_item(1) }
+
+      expect(cart.items.count).to be 2
+    end
+
   end
+  
   describe "Advance function" do
     
   end
