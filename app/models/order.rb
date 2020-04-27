e1b74e544df0be45443f6ad64132375caf2cced4
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-04-27 14:26:14 +0800

    add order number

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,24 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items
+
+  before_create :create_order_num
+
+  private
+
+  def create_order_num
+    self.order_num = num_generator
+  end
+
+  def num_generator
+    year = Date.today.year
+    month = Date.today.month
+    day = Date.today.day
+    code = [*0..9, *'A'..'Z'].sample(6).join
+    today = "#{year}#{"%02d" % month}#{"%02d" % day}#{code}"
+
+    return today
+  end
+
+
 end
